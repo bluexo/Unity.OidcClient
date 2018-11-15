4762e0f8bf2a879724a47af410adca71fe20ceb4
Alvin <bluexo@hotmail.com>
2018-11-15 17:13:41 +0800

    Add IdentityModel.OidcClient as Submudule

---
+++ src/Unity/Assets/Scripts/WindowsBrowser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Assets;
+using IdentityModel.OidcClient.Browser;
+using UnityEngine;
+
+public class WindowsBrowser : MobileBrowser
+{
+    protected override void Launch(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
